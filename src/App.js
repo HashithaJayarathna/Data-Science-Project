fb7f47c87c8381382efefe9f2fa77d5bece44c16
Hasitha Jayarathna <157715874+HashithaJayarathna@users.noreply.github.com>
2024-08-24 21:35:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {BrowserRouter,Route, Routes} from'react-router-dom';
+import './App.css';
+import Signup from './Components/Signup/Signup';
+import Login from './Components/Login/Login';
+import LandingPage from './Components/LandingPage/LandingPage';
+import HomePage from './Components/Home/HomePage';
+import CAdvisor from './Components/CAdvisor/cAdvisor';
+import ResponseTimes from './Components/ResponseTimes/ResponseTimes';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+            <Route index element = {<LandingPage/>}/>
+            <Route path = "/signup" element = {<Signup/>}/>
+            <Route path = "/login" element = {<Login/>}/>
+            <Route path = "/Home" element = {<HomePage/>}/>
+            <Route path = "/CAdvisor" element = {<CAdvisor/>}/>
+            <Route path = "/ResponseTimes" element = {<ResponseTimes/>}/>
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
